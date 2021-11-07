108b1a7e074e9e4c7fc4e6968a16b2f155cf05da
hemantmm <hemant29mehta@gmail.com>
2021-11-07 20:47:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+var a;
+function show_hide(){
+    if(a==1)
+    {
+        document.getElementById("btn").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("btn").style.display="none";
+        return a=1;
+    }
+}
+
+var a;
+function show_hide1(){
+    if(a==1)
+    {
+        document.getElementById("btn1").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("btn1").style.display="none";
+        return a=1;
+    }
+}
+
+var a;
+function show_hide2(){
+    if(a==1)
+    {
+        document.getElementById("btn2").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("btn2").style.display="none";
+        return a=1;
+    }
+}
+
+var a;
+function show_hide3(){
+    if(a==1)
+    {
+        document.getElementById("btn3").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("btn3").style.display="none";
+        return a=1;
+    }
+}
+
+var a;
+function show_hide4(){
+    if(a==1)
+    {
+        document.getElementById("btn4").style.display="inline";
+        return a=0;
+    }
+
+    else
+    {
+        document.getElementById("btn4").style.display="none";
+        return a=1;
+    }
+}
